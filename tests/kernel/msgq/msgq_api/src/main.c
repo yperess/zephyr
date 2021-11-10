135e8e829953d15ecbf1af487b0b41db690c6c0a
Yuval Peress <peress@google.com>
2021-11-10 11:00:34 -0700

    tests: msgq_api: Update to use new ztest APIs
    Split the test suite into 2:
    1. Default with no changes
    2. Adds before/after function to account for 1cpu tests
    
    Signed-off-by: Yuval Peress <peress@google.com>

--- tests/kernel/msgq/msgq_api/src/main.c
+++ tests/kernel/msgq/msgq_api/src/main.c
@@ -12,38 +12,6 @@
  */
 
 #include <ztest.h>
-extern void test_msgq_thread(void);
-extern void test_msgq_thread_overflow(void);
-extern void test_msgq_isr(void);
-extern void test_msgq_put_fail(void);
-extern void test_msgq_get_fail(void);
-extern void test_msgq_purge_when_put(void);
-extern void test_msgq_attrs_get(void);
-extern void test_msgq_alloc(void);
-extern void test_msgq_pend_thread(void);
-extern void test_msgq_empty(void);
-extern void test_msgq_full(void);
-#ifdef CONFIG_USERSPACE
-extern void test_msgq_user_thread(void);
-extern void test_msgq_user_thread_overflow(void);
-extern void test_msgq_user_put_fail(void);
-extern void test_msgq_user_get_fail(void);
-extern void test_msgq_user_attrs_get(void);
-extern void test_msgq_user_purge_when_put(void);
-#else
-#define dummy_test(_name) \
-	static void _name(void) \
-	{ \
-		ztest_test_skip(); \
-	}
-
-dummy_test(test_msgq_user_thread);
-dummy_test(test_msgq_user_thread_overflow);
-dummy_test(test_msgq_user_put_fail);
-dummy_test(test_msgq_user_get_fail);
-dummy_test(test_msgq_user_attrs_get);
-dummy_test(test_msgq_user_purge_when_put);
-#endif /* CONFIG_USERSPACE */
 
 #ifdef CONFIG_64BIT
 #define MAX_SZ	256
@@ -59,6 +27,21 @@ extern struct k_sem end_sema;
 extern struct k_thread tdata;
 K_THREAD_STACK_EXTERN(tstack);
 
+static void msgq_api_1cpu_before(void *data)
+{
+	ARG_UNUSED(data);
+	z_test_1cpu_start();
+}
+
+static void msgq_api_1cpu_after(void *data)
+{
+	ARG_UNUSED(data);
+	z_test_1cpu_stop();
+}
+
+ZTEST_SUITE(msgq_api_1cpu, NULL, NULL, msgq_api_1cpu_before, msgq_api_1cpu_after, NULL);
+ZTEST_SUITE(msgq_api, NULL, NULL, NULL, NULL, NULL);
+
 /*test case main entry*/
 void test_main(void)
 {
@@ -67,23 +50,6 @@ void test_main(void)
 
 	k_thread_heap_assign(k_current_get(), &test_pool);
 
-	ztest_test_suite(msgq_api,
-			 ztest_1cpu_unit_test(test_msgq_thread),
-			 ztest_unit_test(test_msgq_thread_overflow),
-			 ztest_user_unit_test(test_msgq_user_thread),
-			 ztest_user_unit_test(test_msgq_user_thread_overflow),
-			 ztest_unit_test(test_msgq_isr),
-			 ztest_1cpu_unit_test(test_msgq_put_fail),
-			 ztest_1cpu_unit_test(test_msgq_get_fail),
-			 ztest_user_unit_test(test_msgq_user_put_fail),
-			 ztest_user_unit_test(test_msgq_user_get_fail),
-			 ztest_unit_test(test_msgq_attrs_get),
-			 ztest_user_unit_test(test_msgq_user_attrs_get),
-			 ztest_1cpu_unit_test(test_msgq_purge_when_put),
-			 ztest_user_unit_test(test_msgq_user_purge_when_put),
-			 ztest_1cpu_unit_test(test_msgq_pend_thread),
-			 ztest_1cpu_unit_test(test_msgq_empty),
-			 ztest_1cpu_unit_test(test_msgq_full),
-			 ztest_unit_test(test_msgq_alloc));
-	ztest_run_test_suite(msgq_api);
+	ztest_run_test_suites(NULL);
+	ztest_verify_all_test_suites_ran();
 }

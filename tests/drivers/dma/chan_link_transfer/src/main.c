37c9ca25d602eb2e114b0d1fb259c3797b088ff2
Aaron Massey <aaronmassey@google.com>
2021-11-14 19:11:52 -0700

    tests: chan_link_transfer: update to new ZTEST APIs
    Updates tests to use the new ZTEST APIs
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I68a014d5a974d70a39a1b569d67b5255ab394acc

--- tests/drivers/dma/chan_link_transfer/src/main.c
+++ tests/drivers/dma/chan_link_transfer/src/main.c
@@ -6,10 +6,7 @@
 
 #include <zephyr.h>
 #include <ztest.h>
-
-extern void test_dma_m2m_chan0_1_major_link(void);
-extern void test_dma_m2m_chan0_1_minor_link(void);
-extern void test_dma_m2m_chan0_1_minor_major_link(void);
+#include <stdbool.h>
 
 #ifdef CONFIG_SHELL
 TC_CMD_DEFINE(test_dma_m2m_chan0_1_major_link)
@@ -28,11 +25,10 @@ SHELL_CMD_REGISTER(test_dma_m2m_chan0_1_minor_major_link, NULL, NULL,
 void test_main(void)
 {
 #ifndef CONFIG_SHELL
-	ztest_test_suite(
-		dma_m2m_link_test,
-		ztest_unit_test(test_dma_m2m_chan0_1_major_link),
-		ztest_unit_test(test_dma_m2m_chan0_1_minor_link),
-		ztest_unit_test(test_dma_m2m_chan0_1_minor_major_link));
-	ztest_run_test_suite(dma_m2m_link_test);
+	return true;
+#else
+	return false;
 #endif
 }
+
+ZTEST_SUITE(dma_m2m_link_test, should_dma_m2m_link_test, NULL, NULL, NULL, NULL);

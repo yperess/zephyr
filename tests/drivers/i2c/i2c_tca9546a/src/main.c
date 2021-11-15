09b6717e03d30221f82953c6ad5fc09a9d942d6a
Aaron Massey <aaronmassey@google.com>
2021-11-14 20:00:07 -0700

    tests: i2c_tca9546a: update to new ZTEST APIs
    Updates tests to use the new ZTEST APIs
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: I1cbac32ae11fcb18b097dbe778495f80b06f63db

--- tests/drivers/i2c/i2c_tca9546a/src/main.c
+++ tests/drivers/i2c/i2c_tca9546a/src/main.c
@@ -31,7 +31,7 @@
  * This test verifies various assert macros provided by ztest.
  *
  */
-static void test_tca9546a(void)
+ZTEST(i2c_tca9546a_test, test_tca9546a)
 {
 	uint8_t buff[1];
 
@@ -45,11 +45,4 @@ static void test_tca9546a(void)
 	i2c_read(i2c1, buff, 1, 0x42);
 }
 
-void test_main(void)
-{
-	ztest_test_suite(framework_tests,
-			 ztest_unit_test(test_tca9546a)
-			 );
-
-	ztest_run_test_suite(framework_tests);
-}
+ZTEST_SUITE(i2c_tca9546a_test, NULL, NULL, NULL, NULL, NULL);

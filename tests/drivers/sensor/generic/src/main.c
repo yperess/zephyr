37bb27314a754da7a294c0dcb0142defaddee18b
Aaron Massey <aaronmassey@google.com>
2021-11-15 16:05:47 -0700

    tests: sensor generic: update to new ZTEST APIs
    Updates tests to use the new ZTEST APIs
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>
    Change-Id: Ica43d270833a96d365aa4a49139caa24c941692a

--- tests/drivers/sensor/generic/src/main.c
+++ tests/drivers/sensor/generic/src/main.c
@@ -103,7 +103,7 @@ static struct trigger_sequence trigger_elements[] = {
  *
  * @see sensor_sample_fetch(), sensor_channel_get()
  */
-void test_sensor_get_channels(void)
+ZTEST(test_sensor_api, test_sensor_get_channels)
 {
 	const struct device *dev;
 	struct sensor_value data;
@@ -189,7 +189,7 @@ static void trigger_handler(const struct device *dev,
  *
  * @see sensor_attr_set(), sensor_trigger_set()
  */
-void test_sensor_handle_triggers(void)
+ZTEST(test_sensor_api, test_sensor_handle_triggers)
 {
 	const struct device *dev;
 	const struct device *dev_no_trig;
@@ -269,7 +269,7 @@ void test_sensor_handle_triggers(void)
  * to convert radians to degrees and degrees to radians.  Verify
  * helper function for converting struct sensor_value to double.
  */
-void test_sensor_unit_conversion(void)
+ZTEST(test_sensor_api, test_sensor_unit_conversion)
 {
 	struct sensor_value data;
 
@@ -311,13 +311,16 @@ void test_sensor_unit_conversion(void)
 #endif
 }
 
-/*test case main entry*/
-void test_main(void)
+static void * test_sensor_api_setup(void)
 {
-	ztest_test_suite(test_sensor_api,
-			 ztest_1cpu_unit_test(test_sensor_get_channels),
-			 ztest_1cpu_unit_test(test_sensor_handle_triggers),
-			 ztest_1cpu_unit_test(test_sensor_unit_conversion));
-
-	ztest_run_test_suite(test_sensor_api);
+	z_test_1cpu_start();
+	return NULL;
 }
+
+static void test_sensor_api_teardown(void *data)
+{
+	z_test_1cpu_stop();
+}
+
+ZTEST_SUITE(test_sensor_api, NULL, test_sensor_api_setup, NULL, NULL,
+	    test_sensor_api_teardown);
